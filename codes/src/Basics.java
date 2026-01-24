f844c9bb01b26c51c981010fc06d938c5200aafa
Nadeem Pasha <nadeem77pasha@gmail.com>
2026-01-24 12:25:57 +0530

    Basics

---
+++ codes/src/Basics.java
@@ -0,0 +1,81 @@
+
+import java.sql.SQLOutput;
+import java.util.*;
+public class Basics {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+//// 1.Write a program to print whether a number is even or odd, also take input from the user.
+//        System.out.println("Enter a number : ");
+//        int n=sc.nextInt();
+//        if(n%2==0){
+//            System.out.println("Even number !");
+//        }else
+//            System.out.println("Odd number ! ");
+
+//// 2.Take name as input and print a greeting message for that particular name.
+//        System.out.println("Enter a name : ");
+//         String name=sc.nextLine();
+//        System.out.println("Hello "+ name + ", Good morning ! ");
+
+//// 3.Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+        //SI=P*T*R
+//        System.out.println("Enter principle : ");
+//        int p=sc.nextInt();
+//        System.out.println("Enter time : ");
+//        int t=sc.nextInt();
+//        System.out.println("Enter rate : ");
+//        int r=sc.nextInt();
+//        int res=p*t*r;
+//        System.out.println("Simple intrest : "+ res);
+
+//// 4. Take 2 numbers as input and print the largest number.
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        if(a>b)
+//            System.out.println("largest number : "+ a);
+//        else if(b>a)
+//            System.out.println("largest number: "+ b);
+//        else
+//            System.out.println("Both number are equal .");
+
+//// 5.To calculate Fibonacci Series up to n numbers.
+//        int n=15;
+//        int firstterm=0;
+//        int secondterm=1;
+//        for(int i=0;i<n;i++){
+//            System.out.print(firstterm+ ",");
+//            int nextterm=firstterm+ secondterm;
+//            firstterm=secondterm;
+//            secondterm=nextterm;
+//        }
+
+//// 6.To find out whether the given String is Palindrome or not.
+//        String s="madam";
+//        String rev="";
+//        for(int i=s.length()-1;i>=0;i--){
+//            rev+=s.charAt(i);
+//        }
+//        System.out.println("Reversed string : "+ rev);
+//        if(s.equals(rev))
+//            System.out.println("Palindrome");
+//        else
+//            System.out.println("Not palindrome ! ");
+
+
+//// 7.To find Armstrong Number between two given number.
+//        System.out.println("Enter a number ; ");
+//        int num=sc.nextInt();
+//        int original=num;
+//        int sum=0;
+//        int no_digits=String.valueOf(num).length();
+//        while(num>0){
+//            int dig=num%10;
+//            sum+=Math.pow(dig,no_digits);
+//            num/=10;
+//        }
+//        if(original==sum)
+//            System.out.println(original+ " is Amstrong");
+//        else
+//            System.out.println(original + " is not Amstrong");
+    }
+}
