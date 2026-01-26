9932dc39030e1a59c8886598ec5ee608ece30e53
Nadeem Pasha <nadeem77pasha@gmail.com>
2026-01-26 12:18:10 +0530

    functions

--- codes/src/Conditions_Loops.java
+++ codes/src/Conditions_Loops.java
@@ -10,7 +10,7 @@ public class Conditions_Loops {
 //        return fact;
 //    }
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
 /// 1.Factorial Program In Java
 //        System.out.println("Enter no : ");
@@ -23,17 +23,53 @@ public class Conditions_Loops {
 //        System.out.println("Fact("+n+") : "+fact);
 
 ///  Subtract the Product and Sum of Digits of an Integer
-            int n=sc.nextInt();
-            int product=1;
-            int sum=0;
-            while(n>0){
-                int digit=n%10;
-                product*=digit;
-                sum+=digit;
-                n/=10;
-            }
-            int res=product-sum;
-        System.out.println("P-S : "+ res);
-    }
+//        int n = sc.nextInt();
+//        int product = 1;
+//        int sum = 0;
+//        while (n > 0) {
+//            int digit = n % 10;
+//            product *= digit;
+//            sum += digit;
+//            n /= 10;
+//        }
+//        int res = product - sum;
+//        System.out.println("P-S : " + res);
 
+//// 4. LARGEST OF 3 NUMBERS
+//            int a=sc.nextInt();
+//            int b=sc.nextInt();
+//            int c=sc.nextInt();
+//
+//            if(a>b && b>c)
+//                System.out.println(a+ " is largest");
+//            else if(b>a && b>c)
+//                System.out.println(b+" is largest");
+//            else if(c>a && c>b)
+//                System.out.println(c+" is largest");
+//            else
+//                System.out.println("All are equal ! ");
+
+//// repeating ocurance
+//            int n=1737678767;
+//            int count=0;
+//            while(n>0){
+//                int digit=n%10;
+//                if(digit==7){
+//                    count++;
+//                }
+//                n/=10;
+//            }
+//        System.out.println("7 occurs : "+ count +" times");
+
+
+////REVERSING
+//            int n=123456;
+//            int ans=0;
+//            while(n>0){
+//                int rem=n%10;
+//                n/=10;
+//                ans=ans*10 + rem;
+//            }
+//        System.out.println(ans);
+    }
 }
---
+++ codes/src/Functions.java
@@ -0,0 +1,86 @@
+/// 1.Define two methods to print the maximum and the minimum number respectively among three numbers entered by the user.
+//import java.util.*;
+//public class Functions {
+//    public static int maxi(int n1,int n2,int n3){
+//        int res=Math.max(n1,n2);
+//        int ress=Math.max(res,n3);
+//        return ress;
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter 1st number");
+//        int n1=sc.nextInt();
+//        System.out.println("Enter 2nd number");
+//        int n2=sc.nextInt();
+//        System.out.println("Enter 3rd number");
+//        int n3=sc.nextInt();
+//        System.out.println("Maximum number is : "+ maxi(n1,n2,n3));
+//    }
+//}
+
+/// 2.Write a program that will ask the user to enter his/her marks (out of 100). Define a method that will display grades according to the marks entered as below:
+///        Marks        Grade
+///        91-100         AA
+///        81-90          AB
+///        71-80          BB
+///        61-70          BC
+///        51-60          CD
+///        41-50          DD
+///        <=40          Fail
+//import java.util.*;
+//public class Functions {
+//    public static void grades(int marks){
+//        if(marks>91 && marks<=100)
+//            System.out.println("Grade : AA");
+//        else if(marks>81 && marks<=90)
+//            System.out.println("Grade : AB");
+//        else if(marks>71 && marks<=80)
+//                System.out.println("Grade : BB");
+//        else if(marks>61 && marks<=70)
+//            System.out.println("Grade : BC");
+//        else if(marks>51 && marks<=60)
+//                System.out.println("Grade : CD");
+//        else if(marks>41 && marks<=50)
+//            System.out.println("Grade : DD");
+//        else if(marks<40)
+//            System.out.println("Failed !");
+//        else
+//            System.out.println("Enter correct marks !!");
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter the marks : ");
+//        int marks=sc.nextInt();
+//        grades(marks);
+//    }
+//}
+
+
+/// 3.Write a program to print the factorial of a number by defining a method named 'Factorial'.
+/// Factorial of any number n is represented by n! and is equal to 1 * 2 * 3 * .... * (n-1) *n. E.g.-
+//        4! = 1 * 2 * 3 * 4 = 24
+//        3! = 3 * 2 * 1 = 6
+//        2! = 2 * 1 = 2
+//        Also,
+//        1! = 1
+//        0! = 1
+import java.util.*;
+public class Functions{
+    public static void factorial(int n){
+        if(n==1 || n==0){
+            System.out.println("1");
+        }
+        int product=1;
+        System.out.print(n+"! : ");
+        for(int i=1;i<=n;i++){
+            product*=i;
+            System.out.print(i+" ");
+            if(i<n)
+                System.out.print("* ");
+        }
+        System.out.println(" : "+ product);
+    }
+    public static void main(String[] args) {
+        factorial(5);
+    }
+}
